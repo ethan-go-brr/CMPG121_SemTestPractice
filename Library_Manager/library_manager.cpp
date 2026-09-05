fc6c004569c64237ed3e1e084f926ffd294c64df
Ethan Govender <ethangovender31342018@outlook.com>
2026-09-05 16:59:15 +0200

    Added more functionality

--- Library_Manager/library_manager.cpp
+++ Library_Manager/library_manager.cpp
@@ -26,18 +26,18 @@ Book makeBook(string title, string ID, bool available)
     return books;
 }
 
-string createBookID(Book books[], int count)
+void createBookID(Book books[], int count)
 {
     string title;
     string ID, firstThree;
     int randNum;
 
     title = books[count].title;
-    firstThree = title.substr(0, 3);
+    firstThree = title.substr(0, 3); // would like to make the letters uppercase
     randNum = rand() % 100;
     ID = firstThree + to_string(randNum);
 
-    return ID;
+    books[count].ID = ID;
 }
 
 
@@ -48,6 +48,7 @@ void addNewBook(Book books[], int& count)
 
     do 
     {
+        cin.ignore();
         cout << "Title of the book: ";
         getline(cin, books[count].title);
         
@@ -55,8 +56,7 @@ void addNewBook(Book books[], int& count)
         getline(cin, books[count].author);
 
         // ID Stuff needs to go here
-        ID = createBookID(books, count);
-        books[count].ID = ID;
+        createBookID(books, count);
         // availability will also go here
         books[count].available = true;
         count++;
@@ -67,91 +67,236 @@ void addNewBook(Book books[], int& count)
     } while (tolower(opt) == 'y');
 }
 
-bool isAvailable()
-{
-
-}
 
 void viewBooks(Book books[], int count)
 {
     string ID;
+    if (count < 1)
+    {
+        cout << "There are no books to view!" << endl;
+    }
     cout << "=== All books in the library ===" << endl;
     cout << endl;
-    ID = createBookID(books, count);
     for (int i = 0; i < count; i++)
     {
-        cout << books[count].title << " by " << books[count].author << endl;
-        cout << "Book ID: " << ID << endl;
-        cout << "Availability: " << /*still need to do the availability function*/ << endl;
+        cout << books[i].title << " by " << books[i].author << endl;
+        cout << "Book ID: " << books[i].ID << endl;
+        if (books[i].available)
+        {
+            cout << "Availabiltiy: This book is available to borrow" << endl;
+        }
+        else
+        {
+            cout << "Availability:  This book is currently being borrowed" << endl;
+        }
+        
+        cout << endl;
     }
 }
 
+void borrowBook(Book books[], int count, int& borrowedCount, int& availCount)
+{
+    string search;
+    bool found = false;
+    if (count < 1 || availCount < 1)
+    {
+        cout << "There are no books to borrow!" << endl;
+        return;
+    }
+    cout << "Enter book ID to borrow: ";
+    cin >> search;
+
+    for (int i = 0; i < count; i++)
+    {
+        if (books[i].ID == search)
+        {
+            found = true;
+            books[i].available = false;
+            borrowedCount++;
+            availCount--;
+        }
+    }
+    if (!found)
+    {
+        cout << "Book ID not found" << endl;
+    }
+}
+
+void returnBook(Book books[], int count, int& availCount, int& borrowedCount)
+{
+    string search;
+    bool found = false;
+    if (borrowedCount < 1 || count < 1)
+    {
+        cout << "There are no books to return!" << endl;
+        return;
+    }
+    cout << "Enter book ID to return: ";
+    cin >> search;
+
+    for (int i = 0; i < count; i++)
+    {
+        if (books[i].ID == search)
+        {
+            found = true;
+            books[i].available = true;
+            borrowedCount--;
+            availCount++;
+        }
+    }
+
+    if(!found)
+    {
+        cout << "Book ID not found" << endl;
+    }
+}
+
+void removeBook(Book books[], int& count)
+{
+    bool found = false;
+    cin.ignore();
+
+    if (count < 1)
+    {
+        cout << "There are no books to remove!" << endl;
+        return;
+    }
+    string search;
+    cout << "Enter ID of the book to remove: ";
+    getline(cin, search);
+
+    for (int i = 0; i < count; i++)
+    {
+        if (books[i].ID == search)
+        {
+            found = true;
+            books[i].title = books[i + 1].title;
+            books[i].author = books[i + 1].author;
+            books[i].ID = books[i + 1].ID;
+            books[i].available = books[i + 1].available;
+            count--;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Book ID not found" << endl;
+        return;
+    }
+}
 
 int main()
 {
     srand(time(0));
 
     int choice;
+    int availCount = 0;
+    int borrowedCount = 0;
     int count = 0;
+    const int MAX_SIZE = 50;
+    Book books[MAX_SIZE];
 
-    cout << "Welcome to the Book Manager!" << endl;
-
-    cout << "What would you like to do?" << endl;
-    cout << endl;
-
-    cout << "1. Add a new book" << endl;
-    cout << "2. View all books" << endl;
-    cout << "3. View all available books" << endl;
-    cout << "4. View all borrowed books" << endl;
-    cout << "5. Rent a book" << endl;
-    cout << "6. Return a book" << endl;
-    cout << "7. Remove a book from the shelves" << endl;
-    cout << "8. Exit" << endl;
-
-    cout << "Enter a choice: ";
-    cin >> choice;
-
-    switch (choice)
+    do
     {
-        case 1:
+       cout << "Welcome to the Book Manager!" << endl;
+
+        cout << "What would you like to do?" << endl;
+        cout << endl;
+
+        cout << "1. Add a new book" << endl;
+        cout << "2. View all books" << endl;
+        cout << "3. View all available books" << endl;
+        cout << "4. View all borrowed books" << endl;
+        cout << "5. Borrow a book" << endl;
+        cout << "6. Return a book" << endl;
+        cout << "7. Remove a book from the shelves" << endl;
+        cout << "8. Exit" << endl;
+
+        cout << "Enter a choice: ";
+        cin >> choice;
+
+        switch (choice)
         {
-            break;
+            case 1:
+            {
+                addNewBook(books, count);
+                break;
+            }
+            case 2:
+            {
+                viewBooks(books, count);
+                break;
+            }
+            case 3:
+            {
+                
+                cout << "=== All available books ===" << endl;
+                for (int i = 0; i < count; i++)
+                {
+                    if (books[i].available)
+                    {
+                        cout << books[i].title << " by " << books[i].author << endl;
+                        cout << endl;
+                        availCount++;
+                    }
+                }
+
+                if (availCount < 1)
+                {
+                    cout << "There are no books currently available" << endl;
+                }
+                
+                break;
+            }
+            case 4:
+            {
+                
+                cout << "=== All currently borrowed books ===" << endl;
+                for (int i = 0; i < count; i++)
+                {
+                    if (!books[i].available)
+                    {
+                        cout << books[i].title << " by " << books[i].author << endl;
+                        borrowedCount++;
+                    }
+                }
+
+                if (borrowedCount < 1)
+                {
+                    cout << "No books are currently being borrowed" << endl;
+                }
+                break;
+            }
+            case 5:
+            {
+                borrowBook(books, count, borrowedCount, availCount);
+                break;
+            }
+            case 6:
+            {
+                returnBook(books, count, availCount, borrowedCount);
+                break;
+            }
+            case 7:
+            {
+                removeBook(books, count);
+                break;
+            }
+            case 8:
+            {
+                cout << "Thank you for using The Book Manager! Goodbye." << endl;
+                break;
+            }
+            default:
+            {
+                cout << "Invalid option. Please choose an option between 1 and 8!" << endl;
+                break;
+            }
         }
-        case 2:
-        {
-            break;
-        }
-        case 3:
-        {
-            break;
-        }
-        case 4:
-        {
-            break;
-        }
-        case 5:
-        {
-            break;
-        }
-        case 6:
-        {
-            break;
-        }
-        case 7:
-        {
-            break;
-        }
-        case 8:
-        {
-            cout << "Thank you for using The Book Manager! Goodbye." << endl;
-            break;
-        }
-        default:
-        {
-            cout << "Invalid option. Please choose an option between 1 and 8!" << endl;
-            break;
-        }
-    }
+
+    } while (choice != 8);
+    
+    
     return 0;
 }
 
