80c75e20c4fc3cfb1d7406b1c5f408918f7e21f9
Ethan Govender <ethangovender31342018@outlook.com>
2026-09-05 13:34:20 +0200

    Added more functions, some still incomplete (logic needs to be worked out)

--- Library_Manager/library_manager.cpp
+++ Library_Manager/library_manager.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <cstring>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ struct Book
 {
     string title;
     string ID;
+    string author;
     bool available;
 
 };
@@ -24,15 +26,73 @@ Book makeBook(string title, string ID, bool available)
     return books;
 }
 
-void addNewBook(Book books[], int count)
+string createBookID(Book books[], int count)
 {
-    
+    string title;
+    string ID, firstThree;
+    int randNum;
+
+    title = books[count].title;
+    firstThree = title.substr(0, 3);
+    randNum = rand() % 100;
+    ID = firstThree + to_string(randNum);
+
+    return ID;
 }
+
+
+void addNewBook(Book books[], int& count)
+{
+    char opt;
+    string ID;
+
+    do 
+    {
+        cout << "Title of the book: ";
+        getline(cin, books[count].title);
+        
+        cout << "Author of the book: ";;
+        getline(cin, books[count].author);
+
+        // ID Stuff needs to go here
+        ID = createBookID(books, count);
+        books[count].ID = ID;
+        // availability will also go here
+        books[count].available = true;
+        count++;
+
+        cout << "Would you like to add another book? (y/n): ";
+        cin >> opt;
+
+    } while (tolower(opt) == 'y');
+}
+
+bool isAvailable()
+{
+
+}
+
+void viewBooks(Book books[], int count)
+{
+    string ID;
+    cout << "=== All books in the library ===" << endl;
+    cout << endl;
+    ID = createBookID(books, count);
+    for (int i = 0; i < count; i++)
+    {
+        cout << books[count].title << " by " << books[count].author << endl;
+        cout << "Book ID: " << ID << endl;
+        cout << "Availability: " << /*still need to do the availability function*/ << endl;
+    }
+}
+
+
 int main()
 {
     srand(time(0));
 
     int choice;
+    int count = 0;
 
     cout << "Welcome to the Book Manager!" << endl;
 
